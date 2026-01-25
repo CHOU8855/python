9cc9b4c6b685239adc274eff13d9fc1eba6cfceb
CHOU8855 <Hami_choudhry1@icloud.com>
2026-01-25 16:30:16 +0000

    Add files via upload

---
+++ Algebra.py
@@ -0,0 +1,8 @@
+i = int(input('enter a number: '))
+if (i < 15):
+    print ('i is smaller than 15')
+    print ('im an if block')
+else:
+    print('i is greater than 15')
+    print('im an else block' )
+    print ('im not an if nor an else block') 
