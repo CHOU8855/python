74bb24bc2c2daed950fd9cd26579f306847ab0de
CHOU8855 <Hami_choudhry1@icloud.com>
2026-03-21 11:43:35 +0000

    Add files via upload

---
+++ Rajjustememberyourfriendsbirthdaylikehonestlythatsbeingabadfriend.py
@@ -0,0 +1,11 @@
+Petina = '27 of the 7th 2007'
+rulelele = '12th of the 9th 1834'
+frghuojia = '23 of the 439 32bc'
+efwd = '32 of the 978 2134bc'
+bjhdf = '349 of the 43950 3924'
+
+print('petinas birthday is in',Petina)
+print('rulele birthday is in',rulelele)
+print('frghuojia birthday is in',frghuojia)
+print('efwd birthday is in',efwd)
+print('bjhdf birthday is in',bjhdf)
