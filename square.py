ae98932dba78431c4b01a149c1b9e844939fa8db
CHOU8855 <Hami_choudhry1@icloud.com>
2026-04-25 12:35:22 +0100

    Add files via upload

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.Screen = turtle.bgcolor('purple')
+board = turtle.Turtle()
+
+board.forward (100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+board.right(90)
+board.forward(100)
+turtle.done
