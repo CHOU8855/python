704b8a4deeb89d6cfa35597295043416ee600870
CHOU8855 <Hami_choudhry1@icloud.com>
2026-05-10 18:49:19 +0100

    Add files via upload

---
+++ 2015factors.py
@@ -0,0 +1,21 @@
+for n in range (20):
+    if n%20 == 0:
+        print ('twist')
+
+
+    elif n%15 == 0:
+        pass
+
+    elif n%5 ==0:
+        print('fizz')
+
+    elif n%3 == 0:
+        print ('buzz')
+    
+    else:
+        print(n)
+
+
+
+   
+        
