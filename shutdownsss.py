277df1fd5657e09fb5cb7aef7ff5895202a00208
CHOU8855 <Hami_choudhry1@icloud.com>
2026-05-10 18:09:43 +0100

    Add files via upload

---
+++ shutdownsss.py
@@ -0,0 +1,10 @@
+e = input('Would you like to shutdown? ')
+
+if e == 'yes':
+    print('Shutting down...')
+
+elif e == 'no':
+    print('Shutting down aborted.')
+
+else:
+    print('Sorry, please give a valid answer.')
