9ff40f552807030401920fbdcde5174db751226c
CHOU8855 <Hami_choudhry1@icloud.com>
2026-05-23 12:21:42 +0100

    Add files via upload

---
+++ multipleexceptions.py
@@ -0,0 +1,13 @@
+try:
+    num1, num2 = eval(input('enter two numbers, seperated by a comma :'))
+    result = num1 / num2
+    print('result is',result)
+
+except ZeroDivisionError:
+    print('division by zero is error!!')
+
+except SyntaxError:
+    print('comma is missing enter numbers seperated by a comma like this 1, 2')
+
+except:
+    print('wrong input')
