bb37726ba53a84e6c86ecd1818ac16e0a35bdc45
CHOU8855 <Hami_choudhry1@icloud.com>
2026-05-30 12:43:38 +0100

    Add files via upload

---
+++ mathsymbolsandequations.py
@@ -0,0 +1,13 @@
+import math
+
+print('the ceiling and floor of the value 23.56 are: ' + str(math.ceil(23.56)) + ',' + str
+    (math.floor(23.56)))
+
+x = 10
+y = -15
+
+print('absolute value of -96 and 56 are: ' + str(math.fabs(-96)) +',' + str(math.fabs(56)))
+
+print('the value of x after copying the sign from y is: ' + str(math.copysign(x, y)))
+
+print('the value of gcd oof 24 and 56 : ' + str(math.gcd(24,56)))
