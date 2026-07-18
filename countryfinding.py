cffe5b2c4e0ab3546155e3b1f2468614d469b85c
CHOU8855 <Hami_choudhry1@icloud.com>
2026-07-18 12:42:58 +0100

    Add files via upload

---
+++ countryfinding.py
@@ -0,0 +1,16 @@
+country_code = {'India' : '0091',
+                'saint lucia' : '0025',
+                'dominican republic': '0038',
+                'equitorial guinea' : '0039',
+                'barbados' : '0392',
+                'western saharia' : '0068',
+                'Kazakhstan' : '0067',
+                'Democratic Republic Of Congo' : '0041'}
+
+print('Country code for India -')
+print(country_code.get('India', 'Not Found'))
+      
+
+print('country code for botswanna -')
+print(country_code.get('botswanna', 'Not found'))
+                
