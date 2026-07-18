ec6f42af1b9dd26e78f2910b387f045fb390444b
CHOU8855 <Hami_choudhry1@icloud.com>
2026-07-18 12:22:27 +0100

    Add files via upload

---
+++ studentdataandacloneofsara.py
@@ -0,0 +1,19 @@
+student_data = {
+'id1': {'name': 'Sara', 'class': 'V', 'subject intergration': 'english, math, science,'},
+'id2': {'name': 'David', 'class': 'V', 'subject intergration': 'english, math, science,'},
+'id3': {'name': 'Sara', 'class': 'V', 'subject intergration': 'english, math, science,'},
+'id4': {'name': 'Surya', 'class': 'V', 'subject intergration': 'english, math, science,'}}
+
+result = {}
+seen_keys = []
+
+for student_id, details in student_data.items():
+    unique_key = (details['name'], details['class'], details['subject intergration'])
+
+    if unique_key not in seen_keys:
+        seen_keys.append(unique_key)
+        result[student_id] = details
+
+
+for k, v in result.items():
+    print(k, ':', v)
