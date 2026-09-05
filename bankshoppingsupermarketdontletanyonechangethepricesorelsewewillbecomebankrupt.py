2a8039594b4823763056f4c07cd0e193469b596e
CHOU8855 <Hami_choudhry1@icloud.com>
2026-09-05 12:24:58 +0100

    Add files via upload

---
+++ bankshoppingsupermarketdontletanyonechangethepricesorelsewewillbecomebankrupt.py
@@ -0,0 +1,21 @@
+class Computer:
+
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print('Selling Price: {}'.format(self.__maxprice))
+
+    def setMaxPrice(self, price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+
+c.__maxprice = 900
+c.sell()
+
+
+c.setMaxPrice(506506)
+c.sell()
